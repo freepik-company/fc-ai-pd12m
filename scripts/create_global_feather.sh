f0e72cea6d5eb4f830b2d192d6d035802fb1f882
Daniel Verdu <dverdu@freepik.com>
2024-11-21 16:57:54 +0100

    fix: Update files

--- scripts/create_global_feather.sh
+++
@@ -1,8 +0,0 @@
-export AWS_CONFIG_FILE=".aws/config"
-
-python src/fc_ai_pd12m/create_global_feather.py \
-    --input_folder "s3://fc-gra-alejandria/ds/public/PD12M" \
-    --output_folder "output/PD12M" \
-    --image_path_column "image_path" \
-    --image_extension ".jpg" \
-    --num_workers 64
